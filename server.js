ed10bbbf0f74cd8fc156e3b0fb0e1ed1e7070618
Mitron <mitron6@gmail.com>
2016-12-02 22:31:16 +0000

    Passed all manual tests

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
+app.get('*', function(req, res) {
+    
+    var input = decodeURI(req.path.slice(1));
+
+    var unix = null;
+    // if only digits provided, assume unix time stamp
+    if (input.match(/^[0-9]+$/) !== null) {
+        unix = Number(input);   
+    } else {
+        unix = Date.parse(input); 
+        if (isNaN(unix)) {
+            unix = null;
+        }
+    }
+    
+    var natural = null;
+    if (unix !== null) {
+        var locale = 'en-US';
+        var format = {day: 'numeric', month: 'long', year: 'numeric'};
+        natural = new Date(Number(unix)).toLocaleDateString(locale, format);   
+    }
+    
+    res.send({unix: unix, natural: natural});
 });
 
 app.listen(8080, function() {
-    console.log("Example app listening on port 8080!");
+    console.log("Timestamp microservice listening on port 8080!");
 });
